e291274b9a1a72e34f208179e727b357f10550d9
Skaper <skaper.rtt@gmail.com>
2026-07-16 08:50:45 +0000

    Deploying to gh-pages from @ Skaper/skaper.github.io@177926adcc807ed90c9bb04d328fa81f481d2816 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -92,7 +92,7 @@ ninja.data = [{
         title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("https://www.linkedin.com/in/daniil-rostovsky-99457a149", "_blank");
+          window.open("https://www.linkedin.com/in/daniil-rostovskii-99457a149", "_blank");
         },
       },{
         id: 'social-telegram',
